3e361d40aa4b4c7680e4174c9fe5a1b0f8b6e156
shani <shhhaniburde@gmail.com>
2021-12-13 22:04:57 -0500

    commit tree 65197f5f14d6ed2d3a8114301171ead09659c0f5

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks >= 42) {
+        return blocks - 42;
+    } else if (blocks < 42) {
+        return 42 - blocks;
+    };
+};
+
+function distanceFromHqInFeet(blocks) {
+    if (blocks >= 42) {
+        return (blocks - 42)*264;
+    } else if (blocks < 42) {
+        return (42 - blocks)*264;
+    };
+};
+
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        return (start - end)*264;
+    } else if (start < end) {
+        return (end - start)*264;
+    };
+    
+};
+
+function calculatesFarePrice(start, destination) {
+    let feet = (Math.abs(start - destination)*264);
+    if (feet <= 400) {
+        return 0;
+    } else if (feet >= 400 && feet < 2000) {
+        return (feet - 400) * .02;
+    } else if (feet >= 2000 && feet < 2500) {
+        return 25;
+    } else if (feet >= 2500) {
+        return 'cannot travel that far';
+    }
+}
